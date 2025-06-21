9863dcfa514d29fd87085f2aeb909a8539d0b882
NapsAreNice <dperez59@students.avc.edu>
2025-06-20 17:42:02 -0700

    new file:   Assignment W41 Understanding the structure of C++ Reference Website.cpp 	modified:   a.out 	new file:   a01-NapsAreNice 	new file:   hello.drawio 	modified:   main.cpp 	modified:   a01-NapsAreNice (new commits)

---
+++ Assignment W41 Understanding the structure of C++ Reference Website.cpp
@@ -0,0 +1,12 @@
+/*Assignment for W41. Understanding the strucutre of C++
+We will first utilize how to use the cin and cout objects.*/
+#include <iostream> //brings in the library header to utilize the source file. 
+//this is important so it knows how to use cin or cout. 
+
+using namespace std; //lets the code know not to expect std:: before every object in code. 
+//not sure if it is also for all other std:: but will keep it. may need to add it. 
+
+int main()
+{
+    
+}
--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
+//Reference to assignment 2-1 Hello C++
 #include <iostream>
-using namespace std;
+#include <stdio.h> //contains the standard input/output 
+
 int main()
 {
-    cout << "Hello World!" << endl;
-    exit(0);
+    std::cout << "Hello C++" << std::endl; 
+    /*have to use std:: cause namespace std is not included.
+    cout is character output so we are telling program to output Hello C++*/ 
+    puts("Hello C!");
+    //this is another output mode based in stdio.h. 
+    printf("Hello Alice!\n"); //printf means print formatted. we put \n for new line.
+    printf("Hello %s!\n","Bob"); //%s is a place holder for a string. Bob is the string inserted here. new line \n
+
 }
